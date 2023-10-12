b1b1751e543096a1cb615c50abe0f4a17a367518
Bohdan Balanyk <bbalanyk@deloitte.com>
2023-10-12 16:38:56 +0300

    create py file

---
+++ lab1.py
@@ -0,0 +1,17 @@
+
+
+def traverse_zig_zag(matrix):
+    result = [1,2,3]
+    print("test")
+
+    return result
+
+matrix = [
+    [1, 2, 6],
+    [3, 5, 7],
+    [4, 8, 9],
+]
+
+result = traverse_zig_zag(matrix)
+for element in result:
+    print(element)
