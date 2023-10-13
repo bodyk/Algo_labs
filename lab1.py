5759ab57ea0f7bca28db110be8c15622630a7b25
Bohdan Balanyk <bbalanyk@deloitte.com>
2023-10-13 14:33:50 +0300

    finish writing algorithm

--- lab1.py
+++ lab1.py
@@ -1,8 +1,35 @@
-
-
 def traverse_zig_zag(matrix):
-    result = [1,2,3]
-    print("test")
+    result = []
+    row = 0
+    column = 0
+    m = len(matrix)
+    n = len(matrix[0])
+    result.append(matrix[row][column])
+    while row + 1 < m or column + 1 < n:
+        if column + 1 < n:
+            column += 1
+            result.append(matrix[row][column])
+        elif row + 1 < m:
+            row += 1
+            result.append(matrix[row][column])
+        while row + 1 < m and column - 1 >= 0:
+            if row + 1 < m:
+                row += 1
+            if column - 1 >= 0:
+                column -= 1
+            result.append(matrix[row][column])
+        if row + 1 < m:
+            row += 1
+            result.append(matrix[row][column])
+        elif column + 1 < n:
+            column += 1
+            result.append(matrix[row][column])
+        while column + 1 < n and row - 1 >= 0:
+            if column + 1 < n:
+                column += 1
+            if row - 1 >= 0:
+                row -= 1
+            result.append(matrix[row][column])
 
     return result
 
@@ -10,6 +37,12 @@ matrix = [
     [1, 2, 6],
     [3, 5, 7],
     [4, 8, 9],
+]
+
+matrix = [
+    [1, 2, 6, 7, 12],
+    [3, 5, 8, 11, 13],
+    [4, 9, 10, 14, 15],
 ]
 
 result = traverse_zig_zag(matrix)
