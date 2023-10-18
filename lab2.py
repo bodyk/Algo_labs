baaeba9436c2e3a5eecbe46aa0cfb2982925d53a
Bohdan Balanyk <bbalanyk@deloitte.com>
2023-10-18 16:15:35 +0300

    implement iterative approach

--- lab2.py
+++ lab2.py
@@ -1,6 +1,18 @@
-def max_humsters_to_buy(daily_food, count_humsters_on_market, humsters):
-    result = 0
+def max_hamsters(S, C, hamsters):
+    hamsters.sort(key=lambda x: (x[0] + (C-1)*x[1])/x[0])
 
-    
+    count = 0
+    for H, G in hamsters:
+        required_food = H + count * G
+        if S >= required_food:
+            count += 1
+            S -= required_food
+        else:
+            break
 
-    return result
+    return count
+
+S = 7
+C = 3
+hamsters = [[1, 2], [2, 2], [3, 1]]
+print(max_hamsters(S, C, hamsters))
