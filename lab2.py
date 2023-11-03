f39eda6d40c3f54d48393096f0c65a656e6c732a
Bohdan Balanyk <bbalanyk@deloitte.com>
2023-11-03 16:52:32 +0200

    resolve easier lab2 variant

--- lab2.py
+++ lab2.py
@@ -1,18 +1,11 @@
-def max_hamsters(S, C, hamsters):
-    hamsters.sort(key=lambda x: (x[0] + (C-1)*x[1])/x[0])
-
-    count = 0
-    for H, G in hamsters:
-        required_food = H + count * G
-        if S >= required_food:
-            count += 1
-            S -= required_food
+def min_price(product_prices, discount):
+    discounted_count = len(product_prices) / 3
+    min_sum = 0
+    product_prices.sort(reverse=True)
+    for idx, x in enumerate(product_prices):
+        if (idx + 1 <= discounted_count):
+            min_sum += round(x * ((100 - discount) / 100), 2)
         else:
-            break
+            min_sum += x
 
-    return count
-
-S = 7
-C = 3
-hamsters = [[1, 2], [2, 2], [3, 1]]
-print(max_hamsters(S, C, hamsters))
+    return min_sum
--- lab2_tests.py
+++ lab2_tests.py
@@ -1,28 +1,25 @@
 import unittest
-from lab2 import max_hamsters
+from lab2 import min_price
 
-class TestMaxHamsters(unittest.TestCase):
+class TestMinPrice(unittest.TestCase):
 
     def test_case_1(self):
-        S = 7
-        C = 3
-        hamsters = [[1, 2], [2, 2], [3, 1]]
-        expected = 2
-        self.assertEqual(max_hamsters(S, C, hamsters), expected)
+        product_prices = [50, 20, 30, 17, 100]
+        discount = 10
+        expected = 207.0
+        self.assertEqual(min_price(product_prices, discount), expected)
 
     def test_case_2(self):
-        S = 19
-        C = 4
-        hamsters = [[5, 0], [2, 2], [1, 4], [5, 1]]
-        expected = 3
-        self.assertEqual(max_hamsters(S, C, hamsters), expected)
+        product_prices = [1, 2, 3, 4, 5, 6, 7]
+        discount = 100
+        expected = 15.0
+        self.assertEqual(min_price(product_prices, discount), expected)
 
     def test_case_3(self):
-        S = 2
-        C = 2
-        hamsters = [[1, 50000], [1, 60000]]
-        expected = 1
-        self.assertEqual(max_hamsters(S, C, hamsters), expected)
+        product_prices = [1, 1, 1]
+        discount = 33
+        expected = 2.67
+        self.assertEqual(min_price(product_prices, discount), expected)
 
 
 if __name__ == "__main__":
